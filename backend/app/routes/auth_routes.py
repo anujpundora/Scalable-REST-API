9fcc1229c7a20aa6335c6946a3121ad2ad5f6920
Anuj pundora <anuj28279@gmail.com>
2026-03-06 17:48:23 +0530

    feat: implement user authentication with JWT and password hashing

---
+++ backend/app/routes/auth_routes.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
+from app.models.user import User
+from app.schemas.user_schema import UserCreate, UserLogin
+from app.utils.hashing import hash_password, verify_password
+from app.utils.jwt_handler import create_access_token
+
+router = APIRouter(prefix="/api/v1/auth", tags=["Auth"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+
+    hashed_pw = hash_password(user.password)
+
+    new_user = User(
+        email=user.email,
+        password=hashed_pw
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User registered successfully"}
+
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=400, detail="User not found")
+
+    if not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=400, detail="Invalid password")
+
+    token = create_access_token({"sub": db_user.email})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
